88cd10d2864f0c809e2767e587837f55fb886215
Алексей Стребулаев <asmir800@mail.ru>
2020-06-08 09:10:29 +0300

    Исправил имена массивов, стили именования переменных в соответствии с критериями. Код вставки фрагмента вынес в функцию.

--- js/setup.js
+++ js/setup.js
@@ -4,51 +4,51 @@ var WIZARD_NAMES = ['Иван ', 'Хуан Себастьян ', 'Мария ', 
 
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var userDialogElement = document.querySelector('.setup');
+userDialogElement.classList.remove('hidden');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarListElement = userDialogElement.querySelector('.setup-similar-list');
 
-var randomValue = function (min, max) {
+var getRandomValue = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+var similarWizardElement = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
 var wizards = [
   {
-    name: WIZARD_NAMES[randomValue(0, 7)] + WIZARD_SURNAMES[randomValue(0, 7)],
-    coatColor: COAT_COLOR[randomValue(0, 5)],
-    eyesColor: EYES_COLOR[randomValue(0, 4)]
+    name: WIZARD_NAMES[getRandomValue(0, 7)] + WIZARD_SURNAMES[getRandomValue(0, 7)],
+    coatColor: COAT_COLORS[getRandomValue(0, 5)],
+    eyesColor: EYES_COLORS[getRandomValue(0, 4)]
   },
   {
-    name: WIZARD_NAMES[randomValue(0, 7)] + WIZARD_SURNAMES[randomValue(0, 7)],
-    coatColor: COAT_COLOR[randomValue(0, 5)],
-    eyesColor: EYES_COLOR[randomValue(0, 4)]
+    name: WIZARD_NAMES[getRandomValue(0, 7)] + WIZARD_SURNAMES[getRandomValue(0, 7)],
+    coatColor: COAT_COLORS[getRandomValue(0, 5)],
+    eyesColor: EYES_COLORS[getRandomValue(0, 4)]
   },
   {
-    name: WIZARD_NAMES[randomValue(0, 7)] + WIZARD_SURNAMES[randomValue(0, 7)],
-    coatColor: COAT_COLOR[randomValue(0, 5)],
-    eyesColor: EYES_COLOR[randomValue(0, 4)]
+    name: WIZARD_NAMES[getRandomValue(0, 7)] + WIZARD_SURNAMES[getRandomValue(0, 7)],
+    coatColor: COAT_COLORS[getRandomValue(0, 5)],
+    eyesColor: EYES_COLORS[getRandomValue(0, 4)]
   },
   {
-    name: WIZARD_NAMES[randomValue(0, 7)] + WIZARD_SURNAMES[randomValue(0, 7)],
-    coatColor: COAT_COLOR[randomValue(0, 5)],
-    eyesColor: EYES_COLOR[randomValue(0, 4)]
+    name: WIZARD_NAMES[getRandomValue(0, 7)] + WIZARD_SURNAMES[getRandomValue(0, 7)],
+    coatColor: COAT_COLORS[getRandomValue(0, 5)],
+    eyesColor: EYES_COLORS[getRandomValue(0, 4)]
   }
 ];
 
 var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var wizardElement = similarWizardElement.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -57,10 +57,14 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-similarListElement.appendChild(fragment);
+var insertFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  userDialogElement.querySelector('.setup-similar').classList.remove('hidden');
+};
+
+insertFragment();
